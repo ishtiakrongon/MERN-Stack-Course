d01626de83de046f7ac85d159e43012b37106dc2
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-23 23:14:38 +0600

    assignment1

---
+++ Module1/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+// Assignment 1
+
+// Creating a function called multiplicationTable
+
+function multiplicationTable (num) {
+
+    for (let i = 1; i < 11; i++) {
+       var product = num * i;
+       console.log(num + " X " + i + " = " + product);
+    }
+
+}
+
+multiplicationTable(3);
